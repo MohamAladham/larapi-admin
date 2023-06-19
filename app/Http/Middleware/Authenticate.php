492d72bd09d4bd983cd07b4d89eff8293b52c6eb
Mohammed Aladham <m1aladham@gmail.com>
2023-06-19 14:19:27 +0300

    Return HTTP_UNAUTHORIZED status code rather than redirecting to login page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo( $request )
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if ( !$request->expectsJson() )
+        {
+            abort( Response::HTTP_UNAUTHORIZED );
         }
     }
 }
